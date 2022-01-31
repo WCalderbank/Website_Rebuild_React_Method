7d129b7879f8ff85c45de2d9d7034f556c000efe
WCalderbank <wcalderbank99@gmail.com>
2022-01-31 19:36:31 +0000

    functional App and built components

---
+++ rebuild_app/src/Components/Body/Body.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Body() {
+  return (
+    <div>
+      <h2>ADVENTURE AWAITS!</h2>
+    </div>
+  );
+}
+
+export default Body;
---
+++ rebuild_app/src/Components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div>
+      <h3> Lets Get Started!</h3>
+    </div>
+  );
+}
+
+export default Footer;
